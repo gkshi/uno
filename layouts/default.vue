aa76604a9e6adc9a0affa7b80fb7f3e1cde5cff7
George Shinkarev <gkshi@ya.ru>
2019-02-04 16:06:47 +0300

    [EDIT] layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,23 @@
 <template lang="pug">
   div
+    loader(:ready="ready")
     nuxt
 </template>
+
+<script>
+import loader from '@/components/loader'
+
+export default {
+  components: {
+    loader
+  },
+  data () {
+    return {
+      ready: false
+    }
+  },
+  mounted () {
+    this.ready = true
+  }
+}
+</script>
